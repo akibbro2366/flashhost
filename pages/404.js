d2d370d63a13ec01bd3b7f24859fde1457b80cb7
akibbro2366 <trendyy0001@gmail.com>
2026-05-16 23:49:02 +0530

    Create 404.js

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+export default function Custom404() {
+  return (
+    <div className="page-404">
+      <h1>404</h1>
+      <p>This page doesn&apos;t exist yet.</p>
+      <a href="/">Go home</a>
+    </div>
+  );
+}
